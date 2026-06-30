632b470ae6f6e6ffce182d7322db0847cc60df11
Bryan4631 <bavila@utem.cl>
2026-06-30 19:28:42 -0400

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Header, HTTPException, Depends, status
 from sqlalchemy.orm import Session
-from typing import List, Optional
+from typing import Optional
 from uuid import UUID, uuid4
 from datetime import datetime, timezone
 import random
